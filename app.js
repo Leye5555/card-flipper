c05fcca3c77deb1eab9bbb1756669de93551bff3
Leye5555 <aaeon456engineer@gmail.com>
2024-07-25 17:15:53 +0100

    feat: introduced game initializer

---
+++ app.js
@@ -0,0 +1,48 @@
+const cards = [
+  {
+    name: "card1",
+    img: "./assets/img1.jpg",
+    number: 1,
+  },
+  {
+    name: "card2",
+    img: "./assets/img2.jpg",
+    number: 2,
+  },
+  {
+    name: "card3",
+    img: "./assets/img3.jpg",
+    number: 3,
+  },
+  {
+    name: "card4",
+    img: "./assets/img4.jpg",
+    number: 4,
+  },
+  {
+    name: "card5",
+    img: "./assets/img5.jpg",
+    number: 5,
+  },
+  {
+    name: "card6",
+    img: "./assets/img6.jpg",
+    number: 6,
+  },
+  {
+    name: "card7",
+    img: "./assets/img7.jpg",
+    number: 7,
+  },
+  {
+    name: "card8",
+    img: "./assets/img8",
+    number: 8,
+  },
+];
+const player1Canvas = document.getElementById("player1-canvas");
+const player2Canvas = document.getElementById("player2-canvas");
+
+function init() {}
+
+init();
